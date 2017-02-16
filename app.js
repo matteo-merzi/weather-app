7ede41f1deba1b3dd79f531fa3e753fcc33c69de
Matteo Merzi <matteo.merzi@gmail.com>
2017-02-16 21:43:58 +0100

    Moved geocode request in a different file

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const request = require('request');
 const yargs = require('yargs');
 
-const argv = yargs
+const geocode = require('./geocode/geocode');
+
+argv = yargs
     .options({
         a: {
             demand: true,
@@ -14,19 +15,10 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    json: true
-}, (error, response, body) => {
-    if (error) {
-        console.log('Unable to connect to Google servers.');
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log('Unable to find that address.');
-    } else if (body.status === 'OK') {
-        console.log(`Adress: ${body.results[0].formatted_address}`);
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(results, undefined, 2));
     }
 });
