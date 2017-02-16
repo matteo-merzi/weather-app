5eac37f4841082c9c2ac4508463cd777199ca4a1
Matteo Merzi <matteo.merzi@gmail.com>
2017-02-16 21:05:13 +0100

    Added code to request geo data from google api

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=Via%20Buozzi%201%20Peschiera%20del%20garda%20Verona',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    if (error) {
+        console.log('Unable to connect to Google servers.');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find that address.');
+    } else if (body.status === 'OK') {
+        console.log(`Adress: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
 });
