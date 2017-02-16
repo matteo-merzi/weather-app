ab9620ff2f98ae54aef7757ec9cbadee12fad9d7
Matteo Merzi <matteo.merzi@gmail.com>
2017-02-16 21:51:08 +0100

    Played again with dark-sky api

--- playground/dark-sky-request.js
+++ playground/dark-sky-request.js
@@ -4,7 +4,11 @@ request({
     url: 'https://api.darksky.net/forecast/5015a49661c69c761114d5530ba8f199/45.4396385,10.6865449',
     json: true
 },(error, response, body) => {
-    console.log(JSON.stringify(body.currently.temperature, undefined, 2));
+    if (!error && response.statusCode === 200) {
+        console.log(body.currently.temperature);
+    } else {
+        console.log('Unable to fetch weather.');
+    }
 });
 
 
